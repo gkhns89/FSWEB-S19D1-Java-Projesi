4eba2e07a708b9d57e2e4cd26cefac6edb050a2d
gkhns89 <gokhansisman@outlook.com>
2023-10-30 16:59:04 +0300

    Initial commit!

---
+++ src/main/java/com/wit/S191/S191Application.java
@@ -0,0 +1,13 @@
+package com.wit.S191;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S191Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S191Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wit/S191/S191ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wit.S191;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S191ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
